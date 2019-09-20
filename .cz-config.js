5a86c2ac67771e291610016102ae44262c43474a
Jeroen van Veen <jeroen@garage11.tech>
2019-09-20 20:35:08 +0200

    perf(proc): new conventional-commit config

--- .cz-config.js
+++ .cz-config.js
@@ -1,77 +1,45 @@
 // App-specific scope sections.
 const appSections = [
-    { value: 'api', name: 'API-endpoints' },
-    { value: 'express', name: 'Express & HTTP logic' },
-    { value: 'pdf', name: 'PDF rendering logic' },
-    { value: 'puppeteer', name: 'Puppeteer & Pooling logic' }
-]
+    {value: 'vue', name: 'vue:  UI/View code'},
+    {value: 'webrtc', name: 'webrtc:    WebRTC logic'},
+    {value: 'crypto', name: 'crypto:  Cryptography changes'},
+    {value: 'sig11', name: 'sig11:     SIG11 functionality'},
+    {value: 'sip', name: 'sip:     SIP functionality'},
+];
 
 module.exports = {
     types: [
-        {
-            value: 'chore',
-            name: 'chore:    Build system, dependencies & auxiliary commits'
-        },
-        { value: 'docs', name: 'docs:     Documentation updates' },
-        { value: 'feat', name: 'feat:     New feature' },
-        { value: 'fix', name: 'fix:      Bug fix' },
-        { value: 'perf', name: 'perf:     Performance improvement' },
-        {
-            value: 'ref',
-            name:
-                'ref:      Refactor: Structure, readability and high impact code changes'
-        },
-        { value: 'rev', name: 'rev:      Revert: Reverts a previous commit' },
-        {
-            value: 'qa',
-            name: 'qa:       Quality Assurance: CI, tests, linting, formatting'
-        }
+        {value: 'feat', name: 'feat:     New feature'},
+        {value: 'fix', name: 'fix:      Bug fix'},
+        {value: 'docs', name: 'docs:     Documentation only changes'},
+        {value: 'style', name: 'style:    Formatting changes that do not affect the meaning of the code'},
+        {value: 'refactor', name: 'refactor: Code complexity & Readability'},
+        {value: 'perf', name: 'perf:     Performance improvements'},
+        {value: 'test', name: 'test:     Test, linting, CI'},
+        {value: 'chore', name: 'chore:    Process, Build, dependencies & auxiliary commits'}
     ],
     scopeOverrides: {
-        chore: [
-            { value: 'build', name: 'build: Build system adjustments' },
-            { value: 'deps', name: 'deps:  Update Dependencies' }
-        ],
-        docs: [
-            { value: 'code', name: 'code: Code documentation' },
-            { value: 'dev', name: 'dev:  Developer instructions' },
-            { value: 'user', name: 'user: User instructions' }
-        ],
         feat: appSections,
         fix: appSections,
+        docs: [
+            { value: 'dev', name: 'dev:  Developer documentation'},
+            { value: 'user', name: 'user: User documentation'}
+        ],
+        style: [
+            { value: 'js', name: 'js: Javascript-related styling changes'}
+        ],
+        refactor: appSections,
         perf: [
-            { value: 'cpu', name: 'cpu: Improve handling of CPU resources' },
-            { value: 'io', name: 'io:  Less disk IO' },
-            { value: 'mem', name: 'mem: Improve memory footprint' },
-            { value: 'sql', name: 'sql: Less queries, more efficient queries' }
+            {value: 'code', name: 'code:  Code execution improvement'},
+            {value: 'proc', name: 'proc:  Process improvement'}
         ],
-        ref: [
-            {
-                value: 'init',
-                name: 'init:   Major code commits during initial project phase'
-            },
-            { value: 'read', name: 'read:   Improves readability' },
-            { value: 'rewrite', name: 'rewrite: Major code rewrite' },
-            { value: 'structure', name: 'org: Code organisation & structure' }
+        test: [
+            {value: 'ci', name: 'ci:     Continuous Integration'},
+            {value: 'lint', name: 'lint:   Linting rule changes and definitions'}
         ],
-        rev: [
-            { value: 'bug', name: 'bug:   Criticial bug in production' },
-            {
-                value: 'req',
-                name: 'imp:   Incorrect implementation (not in production)'
-            }
-        ],
-        qa: [
-            { value: 'ci', name: 'ci:     Continuous Integration' },
-            {
-                value: 'format',
-                name: 'format: Formatting & low-risk cleanup actions'
-            },
-            { value: 'test', name: 'test:   Tests and Testing code changes' },
-            {
-                value: 'lint',
-                name: 'lint:   Linting rule changes and definitions'
-            }
+        chore: [
+            {value: 'build', name: 'build: Build process (i.e. transpilation)'},
+            {value: 'deps', name: 'deps:  Dependency updates'}
         ]
     },
     messages: {
@@ -79,8 +47,7 @@ module.exports = {
         scope: 'What is the scope of this change:',
         customScope: 'Denote the SCOPE of this change (press enter to skip):\n',
         subject: 'Write a SHORT, IMPERATIVE subject for the change:\n',
-        body:
-            'Optional - Provide a LONGER body to describe the change (press enter to skip):\n',
+        body: 'Provide a LONGER body to describe the change (press enter to skip):\n',
         breaking: 'Describe any BREAKING CHANGES (press enter to skip):\n',
         confirmCommit: 'Are you sure you want to proceed with the commit above?'
     },
@@ -89,7 +56,7 @@ module.exports = {
     allowBreakingChanges: ['feat', 'fix', 'perf', 'refactor'],
     skipQuestions: ['footer'],
     subjectLimit: 100,
-    ticketNumberPrefix: 'SIM-',
-    isTicketNumberRequired: true,
+    ticketNumberPrefix: '#',
+    isTicketNumberRequired: false,
     allowTicketNumber: true
-}
+};
